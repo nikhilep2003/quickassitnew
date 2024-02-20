0561f719619da7263e34c72a17afbf73047adb0d
Nikhil Nath EP <nikhilnathep222@gmail.com>
2024-02-20 15:14:44 +0530

    gradle change

--- lib/user/homePage.dart
+++ lib/user/homePage.dart
@@ -8,11 +8,21 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 import 'package:quickassitnew/constans/text.dart';
+import 'package:quickassitnew/services/booking_service.dart';
 import 'package:quickassitnew/services/location_provider.dart';
+
+import 'package:quickassitnew/user/all_notifications.dart';
+import 'package:quickassitnew/user/edit_profile.dart';
+import 'package:quickassitnew/user/service_by_type.dart';
+import 'package:quickassitnew/widgets/appbutton.dart';
+
+import 'package:quickassitnew/user/servicetype_list_page.dart';
+
 import 'package:quickassitnew/widgets/apptext.dart';
 import 'package:quickassitnew/widgets/customcontainer.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:svg_flutter/svg.dart';
+import 'package:uuid/uuid.dart';
 
 
 import '../../constans/colors.dart';
@@ -33,6 +43,7 @@ class _HomepageState extends State<Homepage> {
   String?name;
   String?email;
   String?phone;
+  String?img;
 
 
   getData() async {
@@ -42,6 +53,7 @@ class _HomepageState extends State<Homepage> {
     name = await _pref.getString('name');
     phone = await _pref.getString('phone');
     uid = await _pref.getString('uid');
+    img = await _pref.getString('imgurl');
 
     setState(() {});
   }
@@ -131,10 +143,10 @@ class _HomepageState extends State<Homepage> {
                         ),
                         InkWell(
                           onTap: (){
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(builder: (context) =>NotificationPage()),
-                            // );
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) =>AllNotifications()),
+                            );
 
                           },
                           child: Icon(
@@ -143,18 +155,7 @@ class _HomepageState extends State<Homepage> {
                             size: 30,
                           ),
                         ),
-                        InkWell(
-                            onTap: (){
-                              // Navigator.push(
-                              //   context,
-                              //   MaterialPageRoute(builder: (context) =>NotificationPage()),
-                              // );
 
-                            },
-                            child: CircleAvatar(
-
-                            )
-                        )
                       ],
                     ),
                     SizedBox(height: 10,),
@@ -177,35 +178,97 @@ class _HomepageState extends State<Homepage> {
                     SizedBox(height: 10,),
 
 
-                    CustomContainer(
-                      ontap: (){
+                    Container(
+                      height: 220,
+                      child: StreamBuilder(
+                          stream: FirebaseFirestore.instance.collection('offers').snapshots(),
+                          builder: (context,snapshot){
+                            if (snapshot.connectionState == ConnectionState.waiting) {
+                              return Center(child: CircularProgressIndicator());
+                            } else if (snapshot.hasError) {
+                              return Text('Error: ${snapshot.error}');
+                            }
 
-                      },
+                            if(snapshot.hasData){
 
-                      height: 185,
-                      padding: EdgeInsets.all(15),
-                      decoration: BoxDecoration(  color: AppColors.contColor1,
+                              final offers=snapshot.data;
+                              return ListView.builder(scrollDirection: Axis.horizontal,
+                                  itemCount: offers!.docs.length,
+                                  itemBuilder: (context,index){
 
-                          borderRadius: BorderRadius.circular(15)),
-                      width: MediaQuery.of(context).size.width,
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 2,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                AppText(data: "50% Offer on Car Washing",size: 22,color: Colors.white,),
-                                AppText(data: "Keep your car shining",size: 16,color: Colors.white,)
+                                    final offer=offers.docs[index];
 
-                              ],
-                            ),
-                          ),
-                          Expanded(child: SvgPicture.asset('assets/svg/carwash.svg'))
-                        ],
+                                    return  Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: CustomContainer(
 
-                      ),
+                                        ontap: () async{
+
+                                        },
+
+                                        height: 230,
+                                        padding: EdgeInsets.all(15),
+                                        decoration: BoxDecoration(  color: AppColors.contColor1,
+
+                                            borderRadius: BorderRadius.circular(15)),
+                                        width: MediaQuery.of(context).size.width,
+                                        child: Row(
+                                          children: [
+                                            Expanded(
+                                              flex: 2,
+                                              child: Column(
+                                                mainAxisAlignment: MainAxisAlignment.start,
+                                                crossAxisAlignment: CrossAxisAlignment.start,
+                                                children: [
+                                                  AppText(data: "${offer['title']}",size: 22,color: Colors.white,),
+                                                  AppText(data: "${offer['description']}",size: 16,color: Colors.white,)
+                                                  ,SizedBox(height: 10,),
+                                                  AppButton(
+                                                      height: 40,
+                                                      width: 150,
+                                                      color: AppColors.btnPrimaryColor,
+                                                      onTap: () async{
+                                                        var bookingid=Uuid().v1();
+                                                        BookingService bookingService = BookingService();
+
+                                                        // Replace the following line with your actual offer data retrieval method
+                                                        Map<String, dynamic> offerData = {'id': offer.id, 'title': offer['title'], 'price': offer['price'],'shopid':offer['shopid'],'bookingid':bookingid};
+
+                                                        // Show Date and Time Picker
+                                                        await _showDateTimePicker(context);
+
+                                                        // Check if the user selected a date and time
+                                                        if (selectedDateTime != null) {
+                                                          // Check if a booking already exists for the selected date and time
+                                                          bool bookingExists = await bookingService.checkBookingExists(offerData['id'], selectedDateTime!,);
+
+                                                          if (bookingExists) {
+                                                            // Display a message or take appropriate action
+                                                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Booking Already Exists for the selected date and Time")));
+                                                          } else {
+                                                            // Book Offer
+                                                            await bookingService.bookOffer(uid!, offerData, selectedDateTime!).then((value) {
+                                                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Booking Done")));
+                                                            });
+
+
+                                                          }
+                                                        }
+
+                                                      }, child: AppText(data: "Book Now",color: Colors.white,))
+                                                ],
+                                              ),
+                                            ),
+                                            Expanded(child: SvgPicture.asset('assets/svg/carwash.svg'))
+                                          ],
+
+                                        ),
+                                      ),
+                                    );
+                                  });
+                            }
+                            return Center(child: CircularProgressIndicator(),);
+                          }),
                     ),
 
                     SizedBox(height: 10,),
@@ -229,7 +292,19 @@ class _HomepageState extends State<Homepage> {
                                 decoration: BoxDecoration(
                                     color: AppColors.contColor2,
                                     borderRadius: BorderRadius.circular(10)),
-                                ontap: () {},
+                                ontap: () {
+
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>ShopList(selectionData: service['title'],city:locationCity ,uid: uid,)));
+
+                                  final Map<String,dynamic>data={
+
+                                  "type":"${service['title']}",
+                                  'location':locationCity,
+                                  };
+
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>ViewServicebyType(data: data,)));
+
+                                },
                                 height: 150,
                                 width: 120,
                                 child: Column(
@@ -252,7 +327,7 @@ class _HomepageState extends State<Homepage> {
 
                     SizedBox(height: 10,),
                     Text(
-                      "Select Vehicle Type",
+                      "Select Service Type",
                       style: themedata.textTheme.displayMedium,
                     ),
                     SizedBox(
@@ -271,7 +346,15 @@ class _HomepageState extends State<Homepage> {
                                 decoration: BoxDecoration(
                                     color: AppColors.contColor2,
                                     borderRadius: BorderRadius.circular(10)),
-                                ontap: () {},
+                                ontap: () {
+
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>ShopList(selectionData: service['title'],city:locationCity ,uid: uid,)));
+
+
+
+
+
+                                },
                                 height: 150,
                                 width: 120,
                                 child: Column(
@@ -301,5 +384,48 @@ class _HomepageState extends State<Homepage> {
     );
 
 
+  }
+  DateTime? selectedDateTime;
+  Future<void> _showDateTimePicker(BuildContext context) async {
+    // 3. Show Date Picker
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime.now().add(Duration(days: 365)), // Set an appropriate end date
+    );
+
+    if (pickedDate == null) {
+      // User canceled the date picker
+      return;
+    }
+
+    // 4. Show Time Picker
+    TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+
+    if (pickedTime == null) {
+      // User canceled the time picker
+      return;
+    }
+
+    // Combine date and time
+    DateTime combinedDateTime = DateTime(
+      pickedDate.year,
+      pickedDate.month,
+      pickedDate.day,
+      pickedTime.hour,
+      pickedTime.minute,
+    );
+
+    // Update the selectedDateTime in the state
+    setState(() {
+      selectedDateTime = combinedDateTime;
+    });
+
+    // Now you can use 'selectedDateTime' for further processing
+    print('Selected Date and Time: $selectedDateTime');
   }
 }
